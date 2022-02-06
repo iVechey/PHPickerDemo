71fbc09a2fd29ec97b0894b313cc0748fd94cce0
iVechey <62487905+iVechey@users.noreply.github.com>
2022-02-05 17:42:30 -0800

    Update PhotoPickerModel.swift

--- starter/PHPickerDemo/PHPickerDemo/PhotoPickerModel.swift
+++ starter/PHPickerDemo/PHPickerDemo/PhotoPickerModel.swift
@@ -5,4 +5,42 @@
 //  Created by Gabriel Theodoropoulos.
 //
 
-import Foundation
+import SwiftUI
+import Photos
+
+struct PhotoPickerModel: Identifiable {
+  enum MediaType {
+    case photo, video, livePhoto
+  }
+  var id: String
+  var photo: UIImage? 
+  var url: URL? 
+  var livePhoto: PHLivePhoto? 
+  var mediaType: MediaType = .photo
+  
+  init(with photo: UIImage) {
+    id = UUID().uuidString
+    self.photo = photo
+    mediaType = .photo
+  }
+  init(with videoURL: URL) {
+    id = UUID().uuidString
+    url = videoURL
+    mediaType = .video
+  }
+ 
+  init(with livePhoto: PHLivePhoto) {
+    id = UUID().uuidString
+    self.livePhoto = livePhoto
+    mediaType = .livePhoto
+  }
+}
+
+class PickedMediaItems: ObservableObject {
+  @Published var items = [PhotoPickerModel]()  
+  
+  func append(item: PhotoPickerModel) { //use this method for taking a photo as well
+    items.append(item)  
+  } 
+}
+
