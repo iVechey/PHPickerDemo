733ba0d408777c4e19e23d3aec9f289811931316
iVechey <62487905+iVechey@users.noreply.github.com>
2022-02-05 17:07:23 -0800

    Update PhotoPicker.swift

--- starter/PHPickerDemo/PHPickerDemo/PhotoPicker.swift
+++ starter/PHPickerDemo/PHPickerDemo/PhotoPicker.swift
@@ -5,4 +5,42 @@
 //  Created by Gabriel Theodoropoulos.
 //
 
-import Foundation
+import SwiftUI
+import PhotosUI
+
+
+struct PhotoPicker: UIViewControllerRepresentable {
+    typealias UIViewControllerType = PHPickerViewController
+  
+  
+ 
+    func makeUIViewController(context: Context) -> PHPickerViewController {
+      var config = PHPickerConfiguration()
+      config.filter = .images
+      config.selectionLimit = 20
+      config.preferredAssetRepresentationMode = .current
+      
+      let controller = PHPickerViewController(configuration: config)
+      controller.delegate = context.coordinator
+      return controller
+    }
+ 
+    func updateUIViewController(_ uiViewController: PHPickerViewController, context: Context) {
+      
+    }
+  
+    func makeCoordinator() -> Coordinator {
+      Coordinator(with: self) 
+    }
+  
+    class Coordinator {
+      var photoPicker: PhotoPicker
+      
+      init(with photoPicker: PhotoPicker) {
+        self.photoPicker = photoPicker 
+      }
+      
+    }
+}
+
+
