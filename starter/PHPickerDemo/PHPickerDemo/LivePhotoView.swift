216dae1c6e9b21fd761512581066de7b8b2bc74f
iVechey <62487905+iVechey@users.noreply.github.com>
2022-02-07 18:26:11 -0800

    Update LivePhotoView.swift

--- starter/PHPickerDemo/PHPickerDemo/LivePhotoView.swift
+++ starter/PHPickerDemo/PHPickerDemo/LivePhotoView.swift
@@ -5,4 +5,21 @@
 //  Created by Gabriel Theodoropoulos.
 //
 
-import Foundation
+import SwiftUI
+import PhotosUI
+
+struct LivePhotoView: UIViewRepresentable {
+  var livePhoto: PHLivePhoto
+  typealias UIViewType = PHLivePhotoView
+  
+  func makeUIView(context: Context) -> PHLivePhotoView {
+    let livePhotoView = PHLivePhotoView()
+    livePhotoView.livePhoto = livePhoto
+    livePhotoView.startPlayback(with: .hint) //this will tell user that this is a live photo
+    return livePhotoView
+  }
+  
+  func updateUIView(_ uiView: PHLivePhotoView, context: Context) { //necessary to have but don't need anything in the function
+    
+  }
+}
